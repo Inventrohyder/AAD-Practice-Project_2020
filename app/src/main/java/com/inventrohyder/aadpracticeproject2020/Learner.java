1cc9ab50b2636079142ab68af330dab508ea4487
Inventrohyder <22187034+Inventrohyder@users.noreply.github.com>
2020-09-13 00:30:40 +0300

    ✨ feat: Add a Learner class

---
+++ app/src/main/java/com/inventrohyder/aadpracticeproject2020/Learner.java
@@ -0,0 +1,31 @@
+package com.inventrohyder.aadpracticeproject2020;
+
+public class Learner {
+    private String name;
+    private String country;
+    private String badgeUrl;
+
+    private int hours;
+
+    private int score;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getBadgeUrl() {
+        return badgeUrl;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
