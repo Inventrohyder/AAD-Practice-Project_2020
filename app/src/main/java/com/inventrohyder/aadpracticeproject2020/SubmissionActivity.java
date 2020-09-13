3c098eac6a414e93dc14198c889bc6820e6ad0c4
Inventrohyder <22187034+Inventrohyder@users.noreply.github.com>
2020-09-13 08:57:54 +0300

    ️💄 feat: add the up button

--- app/src/main/java/com/inventrohyder/aadpracticeproject2020/SubmissionActivity.java
+++ app/src/main/java/com/inventrohyder/aadpracticeproject2020/SubmissionActivity.java
@@ -3,6 +3,9 @@ package com.inventrohyder.aadpracticeproject2020;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import java.util.Objects;
 
 public class SubmissionActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class SubmissionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submission);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
     }
 }
