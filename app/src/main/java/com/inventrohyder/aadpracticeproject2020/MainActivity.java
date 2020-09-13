6c24706cf1cd9be9ec16b9aed981195c0147172f
Inventrohyder <22187034+Inventrohyder@users.noreply.github.com>
2020-09-13 03:15:37 +0300

    ♻️ feat: extend MainActivity from FragmentActivity

--- app/src/main/java/com/inventrohyder/aadpracticeproject2020/MainActivity.java
+++ app/src/main/java/com/inventrohyder/aadpracticeproject2020/MainActivity.java
@@ -2,22 +2,13 @@ package com.inventrohyder.aadpracticeproject2020;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.tabs.TabLayout;
-
-import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager.widget.ViewPager;
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
-
+import com.google.android.material.tabs.TabLayout;
 import com.inventrohyder.aadpracticeproject2020.ui.main.SectionsPagerAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
